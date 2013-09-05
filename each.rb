49a1f5377ee90826a526eb60aadda41820ebc641
John Figueiredo <john@johnfigueiredo.com>
2013-09-04 19:53:23 -0700

    Map done

--- each.rb
+++ each.rb
@@ -1,1 +1,5 @@
-puts "Hello World"
+array = [1,2,3,4,5,6,7,8,9,10]
+
+new_array = array.each { |x| x*2 }
+
+p new_array
--- map.rb
+++ map.rb
@@ -1,1 +1,8 @@
-puts "Hello World"
+#map returns a new array with what has been iterated over it
+# in the case below is is returning x squared in an array up to 10^2
+
+array = [1,2,3,4,5,6,7,8,9,10]
+
+new_array = array.map { |x| x*x }
+
+p new_array
