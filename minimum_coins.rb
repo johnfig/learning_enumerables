6ee3f1b889ca7b750ebf1bb00482bde35c053802
John Figueiredo <john@johnfigueiredo.com>
2014-04-30 16:15:15 -0700

    Added notes to minimum coin

--- minimum_coins.rb
+++ minimum_coins.rb
@@ -1,3 +1,8 @@
+# Problem: See minimum amount of coins it takes to make change
+# Value is an array of coin values # => [1,5,10] or [1,5] or [1,5,10,25]
+# Amount is total amount needed # => 27 is 27 cents which is a 3. 
+# 1 quarter and 2 pennies
+
 def minimum_coins(value, amount)
   coin_count = 0
   while value.count > 0
