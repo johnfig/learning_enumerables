c16261997ee2a8f9e81fd31b9334a9cc58b41f9b
John Figueiredo <john@johnfigueiredo.com>
2014-05-04 11:07:33 -0700

    Adding the longest_improvement algorithm, not totally completed yet

---
+++ longest_improvement.rb
@@ -0,0 +1,11 @@
+# write a function that computes and prints to standard output (stdout) 
+# the longest period of increasing performance for the student that has these grades
+
+def longest_improvement(array)
+  longest_count = 0
+  array.each do |x|
+    
+  end
+end
+
+longest_improvement([9,2,3,4,1,3,5,6,7,1,9])
